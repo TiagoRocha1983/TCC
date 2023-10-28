746ba874de3584bc0ccb08e81517fad5d3c0a5ac
Tiago <tiago_rocha5@outlook.com>
2023-10-27 23:08:11 -0300

    Organizado as pastas e criado uma função em JS

--- conteudoJava/functionCalculo.js
+++ conteudoJava/functionCalculo.js
@@ -1,15 +1,16 @@
 function clicou() {
 
-    const input = document.querySelector('input');
-    const botao = document.querySelector('.botao');
+   const input = document.querySelector('input');
+   const botao = document.querySelector('.botao');
 
-    if (input.getAttribute('type') === 'text'){
-        input.setAttribute('type', 'number');
-        botao.innerText = "Mostrar Calculo";
-    } else {
-        input.setAttribute('type', 'text');
-        botao.innerText = "Nova consulta";
-    }
+   if (input.getAttribute('type') === 'text') {
+       input.setAttribute('type', 'numeric');
+       botao.innerText = 'Novos valores';
 
-    
+   } else {
+    input.setAttribute('type', 'text');
+    botao.innerText = "Nova consulta";
+
+   }
+
 }
